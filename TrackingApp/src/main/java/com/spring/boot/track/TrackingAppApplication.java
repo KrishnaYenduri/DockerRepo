7a071cbc5db6f67c61ac390225501f53bd5e65e3
Yenduri <Yenduri@HP>
2018-02-25 15:59:05 +0530

    Created Tracking App Project.

---
+++ TrackingApp/src/main/java/com/spring/boot/track/TrackingAppApplication.java
@@ -0,0 +1,12 @@
+package com.spring.boot.track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingAppApplication.class, args);
+	}
+}
